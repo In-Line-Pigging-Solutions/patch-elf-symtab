7773fb6307b5587d0f7086e522f260ced31be9ef
jared <27853460+jaredponn@users.noreply.github.com>
2026-05-07 16:38:00 -0600

    Shuffle around Nix + add precommit

--- flake.nix
+++ flake.nix
@@ -14,41 +14,42 @@
       imports = [ ./nix/build.nix ];
       systems = [ "x86_64-linux" ];
 
-  perSystem = { config, pkgs, ... }:
-    let
-      pname = "patch-elf-symtab";
-      src = pkgs.lib.cleanSource ../.;
-    in
-    {
-      packages.${pname} = pkgs.rustPlatform.buildRustPackage {
-        pname = pname;
-        version = "0.1.0";
-        inherit src;
+      perSystem = { config, pkgs, ... }:
+        let
+          pname = "patch-elf-symtab";
+          src = pkgs.lib.cleanSource ./.;
+        in
+        {
+          packages.${pname} = pkgs.rustPlatform.buildRustPackage {
+            pname = pname;
+            version = "0.1.0";
+            inherit src;
 
-        cargoLock = {
-          lockFile = src + "/Cargo.lock";
+            cargoLock = {
+              lockFile = src + "/Cargo.lock";
+            };
+
+            meta = {
+              description = "ELF symbol table patching utility";
+              mainProgram = pname;
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.pre-commit
+              pkgs.rustfmt
+              pkgs.clippy
+              pkgs.rustc
+              pkgs.cargo
+              pkgs.rust-analyzer
+            ];
+            inputsFrom = [ config.packages.${pname} ];
+            env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
         };
-
-        meta = {
-          description = "ELF symbol table patching utility";
-          mainProgram = pname;
-        };
-      };
-
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.rustfmt
-          pkgs.clippy
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.rust-analyzer
-        ];
-        inputsFrom = [ config.packages.${pname} ];
-        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        shellHook = ''
-          ${config.pre-commit.installationScript}
-        '';
-      };
-    };
     };
 }
